877dc8be98eb5554f472aceaaa74dd03455a35b2
shubhamraj82 <raj.shubh8216@gmail.com>
2026-08-12 11:24:36 +0530

    feat: refactor authentication components and restructure layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
-import {
-  ClerkProvider,
-  Show,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs"
+import { ClerkProvider } from "@clerk/nextjs"
 import { shadcn } from "@clerk/ui/themes"
 import { Geist, Geist_Mono } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { Button } from "@/components/ui/button"
 import { Toaster } from "@/components/ui/sonner"
 import { cn } from "@/lib/utils"
 
@@ -40,19 +33,6 @@ export default function RootLayout({
       <body>
         <ClerkProvider appearance={{ theme: shadcn }}>
           <ThemeProvider>
-            <header className="flex h-14 items-center justify-end gap-2 border-b px-6">
-              <Show when="signed-out">
-                <SignInButton mode="modal">
-                  <Button variant="outline">Sign in</Button>
-                </SignInButton>
-                <SignUpButton mode="modal">
-                  <Button>Sign up</Button>
-                </SignUpButton>
-              </Show>
-              <Show when="signed-in">
-                <UserButton />
-              </Show>
-            </header>
             {children}
             <Toaster />
           </ThemeProvider>
